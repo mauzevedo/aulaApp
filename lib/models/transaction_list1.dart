b5ebdf5d793c1b179f5f1ef3d66cbc6b72dc373a
Mauzevedo <mauzevedo1979@gmail.com>
2020-06-25 14:51:23 -0300

    commut inicialgit

---
+++ lib/models/transaction_list1.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import '../models/transaction.dart';
+
+class TransactionList extends StatelessWidget {
+
+  final List<Transaction> transactions;
+
+  TransactionList(this.transactions);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
---
+++ lib/models/transaction_user.dart
@@ -0,0 +1,21 @@
+
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+
+class TransactionUser extends StatefulWidget {
+  @override
+  _TransactionUserState createState() => _TransactionUserState();
+}
+
+class _TransactionUserState extends State<TransactionUser> {
+  
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return ;
+  }
+}
